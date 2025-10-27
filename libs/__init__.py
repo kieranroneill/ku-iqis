e8b22fdfc9c37e634ad2b1b8793166c511a72621
Kieran O'Neill <hello@kieranoneill.com>
2025-10-27 10:36:12 +0100

    feat(electro_data_analysis): adds histogram plots

---
+++ libs/__init__.py
@@ -0,0 +1,3 @@
+from libs.plot_photon_arrival_time_histogram import plot_photon_arrival_time_histogram
+
+__all__ = ["plot_photon_arrival_time_histogram"]
---
+++ libs/plot_photon_arrival_time_histogram.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+from pandas import DataFrame
+
+
+def plot_photon_arrival_time_histogram(data: DataFrame, title: str = "Photon Arrival Time"):
+    """
+    Plots the histogram of photon arrival times.
+
+    Parameters
+    ----------
+    data : pamdas.DataFrame
+        The pandas data to plot.
+    title : str, optional
+        A custom title for the histogram, by default "Photon Arrival Time".
+    """
+    x_label = data.columns[0]
+    y_label = data.columns[1]
+
+    plt.figure(figsize=(8, 5))
+    plt.bar(data[x_label], data[y_label], width=2)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.title(title)
+    plt.tight_layout()
+    plt.show()

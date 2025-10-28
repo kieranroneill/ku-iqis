418181a2deef10999edc4faf5610ba5b5e480be8
Kieran O'Neill <hello@kieranoneill.com>
2025-10-28 18:10:16 +0100

    feat: uses zero attenuation for probability count and adds rates to poster

--- libs/calculate_pair_probability_rate.py
+++ libs/calculate_pair_probability_rate.py
@@ -3,7 +3,7 @@ from pandas import DataFrame
 from libs.convert_picoseconds_to_seconds import convert_picoseconds_to_seconds
 
 
-def calculate_pair_probability_rate(data: DataFrame, repetition_rate: float) -> float:
+def calculate_pair_probability_rate(data: DataFrame, total_photon_count: float) -> float:
     time_difference_label = data.columns[0]
     counts_per_bin_label = data.columns[1]
     pair_peak_min = -500
@@ -11,4 +11,4 @@ def calculate_pair_probability_rate(data: DataFrame, repetition_rate: float) -> 
     pair_data = data[(data[time_difference_label] >= pair_peak_min) & (data[time_difference_label] <= pair_peak_max)]
     total_pair_count = pair_data[counts_per_bin_label].sum()
 
-    return total_pair_count / repetition_rate
+    return total_pair_count / total_photon_count
